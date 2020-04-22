9377b07d538c32b6ff30556117d0fbc57208c5f6
RichardCastillo <armed_deity@hotmail.com>
2020-04-22 03:02:06 -0700

    Initial commit

---
+++ src/edu/whccd/rcastillo/Main.java
@@ -0,0 +1,19 @@
+package edu.whccd.rcastillo;
+
+/* This custom app is for utilizing for and while loops.
+Author: Richard Castillo
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+
+	int i = 0;
+	for(int i = 0; i >= 0; i++)
+	    System.out.println(i);
+    }
+}
