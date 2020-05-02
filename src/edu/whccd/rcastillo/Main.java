c0cbb330e38143483701a6bb471b7875d06695da
RichardCastillo <armed_deity@hotmail.com>
2020-05-02 15:44:28 -0700

    Fixed main problem with Current Number and redid while loop.

--- src/edu/whccd/rcastillo/Main.java
+++ src/edu/whccd/rcastillo/Main.java
@@ -9,33 +9,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
 
-	System.out.print("Enter an Integer between 20-50: ");
-	int limit = sc.nextInt();
+		System.out.print("Enter an Integer between 20-50: ");
+
+		int limit = 0;
+		int currentNumber = 0;
+		Scanner sc = new Scanner(System.in);
+		limit = sc.nextInt();
 
 
 		if (limit < 20 || limit > 50) {
 			System.out.println("invalid input");
 		}
-	for (int i = 1; i <= limit; i++)
-		if (i % 2 == 0) {
-			System.out.println(i);
-		}
+		for (int i = 1; i <= limit; i++)
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
 
-	System.out.println("*************************");
+		System.out.println("*************************");
+		System.out.println("Odd Numbers decrementing");
 
+		currentNumber = limit;
 
-		int currentNumber = 0;
-
-		while (currentNumber > limit) {
+		while (currentNumber > 0) {
+			currentNumber--;
 			if (currentNumber % 2 == 1) {
 				System.out.println(currentNumber);
-				currentNumber--;
 			}
 		}
-
-
 	}
+
+
 
 }
